4d089f81933075378dd3aa484f16ae8c868b76f1
Melody <melodyjacquemin.mj@gmail.com>
2024-03-04 15:53:07 +0100

    résolution des conflits

---
+++ index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import cors from "cors";
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+const port = 8070;
+const host = "127.0.0.1";
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.static(path.join(dirname, "public")));
+app.use(
+  "/favicon.ico",
+  express.static(path.join(dirname, "public", "images", "favicon.png"))
+);
+
+/**
+ * Route GET pour afficher la page d'accueil.
+ * Envoie le fichier 'index.html' comme réponse.
+ *
+ * @param {string} path - Le chemin de la route.
+ * @param {function} callback - La fonction de callback qui gère la requête et la réponse.
+ */
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: path.join(dirname) }, (err) => {
+    if (err) throw new Error(err);
+  });
+});
+
+/**
+ * Route POST pour soumettre des messages.
+ * Récupère le message du corps de la requête et l'envoie en réponse sur une nouvelle page.
+ *
+ * @param {string} path - Le chemin de la route.
+ * @param {function} callback - La fonction de callback qui gère la requête et la réponse.
+ */
+app.post("/comment", (req, res) => {
+  const comment = req.body.message;
+  res.send(comment);
+});
+
+app.listen(port, host, () => {
+  console.info(`Server started @ http://${host}:${port}.`);
+});
